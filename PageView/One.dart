e7115ba6c2033224724d0d3238787b5816bf6618
Mahi <iamaanjna@gmail.com>
2019-08-11 13:18:55 +0530

    Create One.dart

---
+++ PageView/One.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OneClass extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+ 
+ //Return whatever widget you want to return to the main list from here
+    return Center(
+      child: Container(
+        child: Text('Empty Body 0'),
+      ),
+    );
+  }
+
+}
